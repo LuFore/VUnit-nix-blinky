943c23364146240ab9c6b78868821415686cd440
LuFore <83755790+LuFore@users.noreply.github.com>
2025-07-06 22:21:02 +0000

    Add files via upload

--- shell.nix
+++ shell.nix
@@ -1,25 +1,24 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell rec {
-  buildInputs = [
-  	pkgs.nvc
+	buildInputs = [
+		pkgs.nvc
 		pkgs.python310 
-    pkgs.python310Packages.virtualenv
+		pkgs.python310Packages.virtualenv
 	];
-
-  shellHook = ''
-    # Create a virtualenv if it doesn't exist
-    if [ ! -d "venv" ]; then
-      echo "Creating virutal Enviroment"
+	
+	shellHook = ''
+		# Create a virtualenv if it doesn't exist
+		if [ ! -d "venv" ]; then
+			echo "Creating virutal Enviroment"
 			python3 -m venv venv
-    fi
-
+		fi
+		
 		echo "Activating virtual enviroment"
-    source venv/bin/activate
-
-    # If first time, use pip to install requirements
-    echo "Checking/Installing pip packages"
-    pip install --upgrade pip
-    pip install -r requirements.txt
-  '';
+		source venv/bin/activate
+		
+		echo "Checking/Installing pip packages"
+		pip install --upgrade pip
+		pip install -r requirements.txt
+	'';
 }
